49903b5b48df42548e4967a8685d4706d682bdf8
Showherda <showherda@gmail.com>
2020-03-06 14:30:18 +0600

    Update UnionFind.c

--- UnionFind.c
+++ UnionFind.c
@@ -4,15 +4,14 @@ typedef long long ll;
 typedef struct {ll parent, rank;} pair;
 pair set[N];
 void makeset(ll val){
-	set[val].parent=0;
+	set[val].parent=val;
 	set[val].rank=1;
 }
 ll find(ll val){
 	ll tmp=val;
-	while (set[tmp].parent)
+	while (set[tmp].parent!=tmp)
 		tmp=set[tmp].parent;
-	if (tmp)
-		set[val].parent=tmp;
+	set[val].parent=tmp;
 	return tmp;
 }
 void merge(ll a, ll b){
@@ -28,13 +27,4 @@ void merge(ll a, ll b){
 		}
 	}
 	
-}
-int main() {
-	for (ll i=1; i<=10; i++)
-		makeset(i);
-	for (ll i=1; i<10; i++)
-		merge(i, i+1);
-	for (ll i=1; i<=10; i++)
-		printf("%lld\n", find(i));
-	return 0;
 }
