e831ba9da5175acbc498e80e12548e04f3821dc1
Showherda <showherda@gmail.com>
2020-03-06 21:59:38 +0600

    Update primMST.c

--- primMST.c
+++ primMST.c
@@ -8,15 +8,15 @@ typedef struct _list{
     pair p;
     struct _list *next;
 } list;
-bool taken[N];
+bool taken[N], vis[N];
 list *g[N];
 ll n, m, u, v, w, size, mstCost;
 pair pq[N];
 void min_heapify(ll ind){
     ll l=ind*2, r=l+1, lowest=ind;
-    if (l<=size&&(pq[l].v<pq[ind].v||pq[l].w<pq[ind].w))
+    if (l<=size&&(pq[l].v<pq[lowest].v||pq[l].w<pq[lowest].w))
         lowest=l;
-    if (r<=size&&(pq[r].v<pq[ind].v||pq[r].w<pq[ind].w))
+    if (r<=size&&(pq[r].v<pq[lowest].v||pq[r].w<pq[lowest].w))
         lowest=r;
     if (lowest!=ind){
         pair tmp=pq[ind];
@@ -32,21 +32,21 @@ void build_min_heap(void){
 pair extract_min(void){
     pair min=pq[1];
     pq[1]=pq[size];
-    size--;
+    --size;
     min_heapify(1);
     return min;
 }
 void pushh(ll v, ll w){
     ++size;
-    pq[size].v=v, pq[size].w=w;;
+    pq[size].v=v;
+    pq[size].w=w;
     min_heapify(size);
 }
 void process(ll vrtx){
-    taken[vrtx]=true;
-    for (list* a=g[vrtx]; a; a=a->next){
+    taken[vrtx]=1;
+    for (list *a=g[vrtx]; a; a=a->next)
         if (!taken[a->p.v])
             pushh(a->p.v, a->p.w);
-    }
 }
 void pushg(ll u, ll v, ll w){
     list *ptr=(list *) malloc(sizeof(list));
@@ -57,11 +57,12 @@ void pushg(ll u, ll v, ll w){
 }
 int main(){
     scanf("%lld%lld", &n, &m);
-    process(1);
     while (m--){
         scanf("%lld%lld%lld", &u, &v, &w);
+        pushg(v, u, w);
         pushg(u, v, w);
     }
+    process(1);
     while (size){
         pair front=extract_min();
         u=front.v, w=front.w;
