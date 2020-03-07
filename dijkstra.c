e762772edd0170a6029a05a3ec966f877367b2ea
Showherda <showherda@gmail.com>
2020-03-07 19:17:36 +0600

    Update dijkstra.c

--- dijkstra.c
+++ dijkstra.c
@@ -1,105 +1,76 @@
 #include <limits.h>
-#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define N 222222
 typedef long long ll;
-typedef struct _list {
-    ll val;
+typedef struct {ll v, w;} pair;
+typedef struct _list{
+    pair p;
     struct _list *next;
 } list;
-typedef struct {
-    ll d, p, v;
-} vertex;
-typedef struct _pair {
-    ll u, v, w;
-    struct _pair *next;
-} pair;
 list *g[N];
-ll n, m, a, b, w, size;
-pair *edge[N];
-vertex heap[N], arr[N];
-void pushg(ll u, ll v){
-    list *ptr=(list *) malloc(sizeof(list));
-    ptr->val=v;
-    ptr->next=g[u];
-    g[u]=ptr;
-}
-ll hash_fun(ll u, ll v){
-    return ((u*17LL+v*23LL)%N);
-}
-void pushh(ll u, ll v, ll w){
-    ll ind=hash_fun(u, v);
-    pair *ptr=(pair *) malloc(sizeof(pair));
-    ptr->next=edge[ind];
-    ptr->u=u;
-    ptr->v=v;
-    ptr->w=w;
-    edge[ind]=ptr;
-}
+ll n, m, u, v, w, size, dist[N];
+pair pq[N];
 void min_heapify(ll ind){
     ll l=ind*2, r=l+1, lowest=ind;
-    if (l<=size && heap[l].d<heap[ind].d)
+    if (l<=size&&(pq[l].v<pq[ind].v||pq[l].w<pq[ind].w))
         lowest=l;
-    if (r<=size && heap[r].d<heap[ind].d)
+    if (r<=size&&(pq[r].v<pq[ind].v||pq[r].w<pq[ind].w))
         lowest=r;
-    if (lowest!=ind) {
-        vertex tmp=heap[lowest];
-        heap[lowest]=heap[ind];
-        heap[ind]=tmp;
+    if (lowest!=ind){
+        pair tmp=pq[ind];
+        pq[ind]=pq[lowest];
+        pq[lowest]=tmp;
         min_heapify(lowest);
     }
 }
 void build_min_heap(void){
-    for (ll i = size/2; i>=1; i--)
+    for (ll i=size/2; i; i--)
         min_heapify(i);
 }
-vertex extract_min(void){
-    vertex min=heap[1];
-    heap[1]=heap[size];
+pair extract_min(void){
+    pair min=pq[1];
+    pq[1]=pq[size];
     size--;
     min_heapify(1);
     return min;
 }
-void relax(ll u, ll v){
-    ll ind=hash_fun(u, v), w;
-    for (pair *a=edge[ind]; a; a=a->next)
-        if (a->u==u&&a->v==v){
-            w=a->w;
-            break;
-        }
-    if (heap[v].d>heap[u].d+w)
-        heap[v].d=heap[u].d+w, heap[v].p=u,
-        arr[v].d=heap[u].d+w, arr[v].p=u;
+void pushh(ll v, ll w){
+    ++size;
+    pq[size].v=v;
+    pq[size].w=w;
+    min_heapify(size);
 }
-void dijkstra(ll s){
-    while (size){
-        vertex u=extract_min();
-        for (list *a=g[u.v]; a; a=a->next)
-            relax(u.v, a->val);
-    }
+void pushg(ll u, ll v, ll w){
+    list *ptr=(list *) malloc(sizeof(list));
+    ptr->next=g[u];
+    ptr->p.v=v;
+    ptr->p.w=w;
+    g[u]=ptr;
 }
 int main(){
-    scanf("%I64d%I64d", &n, &m);
-    while (m--){
-        scanf("%I64d%I64d%I64d", &a, &b, &w);
-        pushg(a, b);
-        pushg(b, a);
-        pushh(a, b, w);
-        pushh(b, a, w);
-    }
-    for (int i=1; i<=n; i++){
-        heap[++size].v=i;
-        heap[size].d=LLONG_MAX;
-        heap[size].p=-1;
-        arr[size].v=i;
-        arr[size].p=-1;
-        arr[size].d=LLONG_MAX;
-    }
-    build_min_heap();
-    dijkstra(1);
+    scanf("%lld%lld", &n, &m);
     for (ll i=1; i<=n; i++)
-        printf("%I64d %I64d\n", i, arr[i].p);
+        dist[i]=LLONG_MAX;
+    while (m--){
+        scanf("%lld%lld%lld", &a, &b, &w);
+        pushg(a, b, w);
+        pushg(b, a, w);
+    }
+    pushh(1, 0);
+    while (size){
+        pair p=extract_min();
+        ll d=p.w, u=p.v;
+        if (d>dist[u])
+            continue;
+        for (list *a=g[u]; a; a=a->next){
+            pair v=a->p;
+            if (dist[u]+v.w<dist[v.v]){
+                dist[v.v]=dist[u]+v.w;
+                pushh(v.v, v.w);
+            }
+        }
+    }
 }
